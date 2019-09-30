cfbe43c718b5023a8a978efc4d03b5eadc03088a
Kirk Linkletter <kirklinkletter@gmail.com>
2019-09-30 00:58:53 +0000

    new signature

--- lib/src/stanza.dart
+++ lib/src/stanza.dart
@@ -1,6 +1,5 @@
 import 'package:pool/pool.dart' as pl;
 import 'package:postgres/postgres.dart' as pg;
-import 'package:stanza/src/exception.dart';
 import 'package:stanza/src/query.dart';
 import 'package:stanza/src/postgres_credentials.dart';
 import 'package:stanza/src/query_result.dart';
@@ -20,69 +19,72 @@ class _Transaction {
 
 typedef Future<QueryResult<T>> QueryBlock<T>(_Transaction tx);
 
+class StanzaConnection {
+  final PostgresCredentials creds;
+  final pl.Pool pool;
+
+  StanzaConnection(this.creds, this.pool);
+}
 
 class Stanza {
 
-pg.PostgreSQLConnection _connection;
+  pg.PostgreSQLConnection _connection;
+  pl.Pool _pool;
 
-Future<QueryResult<T>> execute<T>(Query query, {bool autoRelease: true}) {
-  return _pool.withResource(() async {
-    _connection = pg.PostgreSQLConnection(
-      _creds.host,
-      _creds.port,
-      _creds.db,
-      username: _creds.username,
-      password: _creds.password
-    );
-    await _connection.open();
-    var result = await _connection.mappedResultsQuery(query.statement(), substitutionValues: query.     substitutionValues);
-    if (autoRelease) {
-      await _connection.close();
+  Stanza._(pg.PostgreSQLConnection this._connection, pl.Pool this._pool);
+
+  factory Stanza(PostgresCredentials creds, {int maxConnections: 25, int timeout: 600}) {
+    final id = '${creds.host}:${creds.port}|${creds.db}';
+    var connection = pg.PostgreSQLConnection(
+        creds.host,
+        creds.port,
+        creds.db,
+        username: creds.username,
+        password: creds.password);
+    if (!_connections.containsKey(id)) {
+      _connections[id] = Stanza._(
+        connection,
+        pl.Pool(maxConnections, timeout: Duration(seconds: timeout))
+      );
     }
-    var queryResult = QueryResult<T>(result);
-    return queryResult;
-  });
-  
-}
+    return _connections[id];
+  }
 
-Future<QueryResult<T>> executeTransaction<T>(QueryBlock queryBlock, {bool autoRelease: true}) {
-  return _pool.withResource(() async {
-    _connection = pg.PostgreSQLConnection(
-      _creds.host,
-      _creds.port,
-      _creds.db,
-      username: _creds.username,
-      password: _creds.password
-    );
-    await _connection.open();
-    QueryResult<T> result = await _connection.transaction((ctx) async {
-      return await queryBlock(_Transaction(ctx));
+
+  Future<QueryResult<T>> execute<T>(Query query, {bool autoRelease: true}) {
+    return _pool.withResource(() async {
+      await _connection.open();
+      var result = await _connection.mappedResultsQuery(query.statement(), substitutionValues: query.     substitutionValues);
+      if (autoRelease) {
+        await _connection.close();
+      }
+      var queryResult = QueryResult<T>(result);
+      return queryResult;
     });
-    if (autoRelease) {
-      await _connection.close();
-    }
-    return result;
-  });
-}
+    
+  }
 
-Future release() async {
-  if (!_connection.isClosed) await _connection.close();
-} 
+  Future<QueryResult<T>> executeTransaction<T>(QueryBlock queryBlock, {bool autoRelease: true}) {
+    return _pool.withResource(() async {
+      await _connection.open();
+      QueryResult<T> result = await _connection.transaction((ctx) async {
+        return await queryBlock(_Transaction(ctx));
+      });
+      if (autoRelease) {
+        await _connection.close();
+      }
+      return result;
+    });
+  }
 
-Stanza._();
+  Future release() async {
+    if (!_connection.isClosed) await _connection.close();
+  } 
 
-factory Stanza.connect() {
-  if (_creds == null) throw QueryException('Database has not been initialized.');
-  return Stanza._();
-}
 
-static void initialize(PostgresCredentials creds, {int maxConnections: 100, int timeout: 600}) {
-  _creds = creds;
-  _pool = pl.Pool(maxConnections, timeout: Duration(seconds: timeout));
-}
-
-static pl.Pool _pool;
-static PostgresCredentials _creds;
+  static Map<String, Stanza> _connections = Map<String, Stanza>();
+  // static pl.Pool _pool;
+  // static PostgresCredentials _creds;
 
 
 }
