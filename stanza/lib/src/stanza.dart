f71ec562a62d67726220dc81ae2275f103b98824
Kirk Linkletter <kirklinkletter@gmail.com>
2020-06-08 13:29:29 -0400

    better initialization api

--- stanza/lib/src/stanza.dart
+++ stanza/lib/src/stanza.dart
@@ -2,6 +2,8 @@ import 'package:pool/pool.dart' as pl;
 import 'package:postgres/postgres.dart' as pg;
 import 'package:stanza/src/connection.dart';
 import 'package:stanza/src/postgres_credentials.dart';
+
+import 'stanza_exception.dart';
 export 'package:stanza/src/update/update_query.dart';
 export 'package:stanza/src/delete/delete_query.dart';
 
@@ -12,6 +14,8 @@ class Stanza {
 
   Stanza._(this._creds, this._pool);
 
+  
+  @deprecated
   factory Stanza(PostgresCredentials creds,
       {int maxConnections = 25, int timeout = 600}) {
     final id = '${creds.host}:${creds.port}|${creds.db}';
@@ -23,6 +27,34 @@ class Stanza {
     return cache;
   }
 
+  factory Stanza.init(PostgresCredentials creds, {int maxConnections = 25, int timeout = 600}) {
+    final id = '${creds.host}:${creds.port}|${creds.db}';
+    if (!_connections.containsKey(id)) {
+      _connections[id] = Stanza._(
+          creds, pl.Pool(maxConnections, timeout: Duration(seconds: timeout)));
+    }
+    var cache = _connections[id];
+    return cache;
+  
+  }
+
+  factory Stanza.getbyDatabase(String host, int port, String database) {
+    final id = '${host}:${port}|${database}';
+    if (!_connections.containsKey(id)) {
+      throw StanzaException('The connection has not been initialized for $host:$port|$database');
+    } else {
+      return _connections[id];
+    }
+  }
+
+  factory Stanza.getByConnectionId(String id) {
+    if (!_connections.containsKey(id)) {
+      throw StanzaException('The connection has not been initialized for $id');
+    } else {
+      return _connections[id];
+    }
+  }
+
   /// Provides a databse connection using the Stanza instance's pooled connections.
   Future<StanzaConnection> connection() async {
     var connection = pg.PostgreSQLConnection(
@@ -31,5 +63,7 @@ class Stanza {
     return StanzaConnection(_pool, connection);
   }
 
-  static Map<String, Stanza> _connections = Map<String, Stanza>();
+  static List<String> get listConnections =>_connections.keys.toList();
+
+  static final Map<String, Stanza> _connections = Map<String, Stanza>();
 }
